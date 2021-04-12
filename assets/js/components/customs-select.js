90fc4b224ed850d29e967545cd62d1af1eff22a0
Audrey Diez <audrey.diez@gmail.com>
2021-04-12 14:47:59 +0200

    Feature: Customs select comportement

--- assets/js/components/customs-select.js
+++ assets/js/components/customs-select.js
@@ -28,53 +28,119 @@ export function setComportment (){
 function toggleSelect(select){
     let selectType = select.getAttribute("value") + "-filters";
     let filtersContainer = document.getElementById(selectType);
-    console.log(select)
 
+
+    // RESET all
+    resetCSSSelect(selectType);
+    moveSelectLeft(selectType);
+
+    // expand / collapse filters
     if(filtersContainer.getAttribute("state") === "collapsed"){
+
+        moveSelectRight(selectType);
+
         filtersContainer.style.display = "flex"
         filtersContainer.setAttribute("state", "expanded");
 
 
         if (getWidth() < 992){
-            select.style.maxWidth ="100%";
+            select.style.minWidth ="100%";
         }
         else if (getWidth() < 1200){
-            select.style.maxWidth ="400px";
+            select.style.minWidth ="400px";
         }
         else {
-            select.style.maxWidth ="600px";
+            select.style.minWidth ="600px";
         }
 
-
-        /*if(filtersContainer.childElementCount < 3){
-            select.style.maxWidth ="600px";
-            Array.from(document.getElementsByClassName("filter")).forEach(filter => {
-                filter.style.flexBasis = "50%";
-            })
-
-        }
-        else if(filtersContainer.childElementCount < 2){
-            select.style.maxWidth ="600px";
-            Array.from(document.getElementsByClassName("filter")).forEach(filter => {
-                filter.style.flexBasis = "100%";
-            })
-
-        }
-        else if(filtersContainer.childElementCount >= 3){
-            select.style.maxWidth ="600px";
-            Array.from(document.getElementsByClassName("filter")).forEach(filter => {
-                filter.style.flexBasis = "33.33333%";
-            })
-
-        }
-*/
     }
     else{
+        // back to normal state
         filtersContainer.style.display = "none"
         filtersContainer.setAttribute("state", "collapsed");
-        select.style.maxWidth ="";
-    }
+        select.style.minWidth ="";
 
+    }
+}
+
+function resetCSSSelect (selectTypeExpanded){
+    Array.from(customsSelect).forEach(select => {
+
+        let selectType = select.getAttribute("value") + "-filters";
+
+        if (selectType != selectTypeExpanded){
+            document.getElementById(selectType).style.display = "none";
+            document.getElementById(selectType).setAttribute("state", "collapsed");
+            select.style.minWidth ="";
+        }
+    })
+}
+
+function moveSelectRight(selectTypeExpanded) {
+
+    Array.from(customsSelect).forEach(select => {
+
+        let selectValue = select.getAttribute("value")+"-filters";
+
+        if (getWidth() <= 992){
+            if (selectTypeExpanded === "ingredients-filters"){
+                document.getElementById(selectTypeExpanded).parentElement.style.zIndex = "2"
+            }
+            if (selectTypeExpanded === "appliances-filters"){
+                document.getElementById(selectTypeExpanded).parentElement.style.zIndex = "2"
+            }
+
+        }
+        else if (getWidth() < 1200 && getWidth() >992){
+            if (selectTypeExpanded === "ingredients-filters"){
+                customsSelect[1].style.left = "420px";
+                customsSelect[2].style.left = "610px";
+            }
+            if (selectTypeExpanded === "appliances-filters"){
+                customsSelect[1].style.left = "";
+                customsSelect[2].style.left = "620px";
+            }
+        }
+        else {
+            if (selectTypeExpanded === "ingredients-filters"){
+                customsSelect[1].style.left = "620px";
+                customsSelect[2].style.left = "810px";
+            }
+            if (selectTypeExpanded === "appliances-filters"){
+                customsSelect[1].style.left = "";
+                customsSelect[2].style.left = "820px";
+            }
+        }
+
+
+    })
+}
+
+function moveSelectLeft(selectTypeExpanded) {
+
+
+    Array.from(customsSelect).forEach(select => {
+
+        let selectValue = select.getAttribute("value")+"-filters";
+
+        if (selectTypeExpanded === "ingredients-filters"){
+            customsSelect[1].style.left = "";
+            customsSelect[2].style.left = "";
+            customsSelect[0].style.zIndex = "0";
+            customsSelect[1].style.zIndex = "0";
+        }
+        if (selectTypeExpanded === "appliances-filters"){
+            customsSelect[1].style.left = "";
+            customsSelect[2].style.left = "";
+            customsSelect[0].style.zIndex = "0";
+            customsSelect[1].style.zIndex = "0";
+        }
+        if (selectTypeExpanded === "ustensils-filters"){
+            customsSelect[1].style.left = "";
+            customsSelect[2].style.left = "";
+        }
+
+    })
 }
 
 
