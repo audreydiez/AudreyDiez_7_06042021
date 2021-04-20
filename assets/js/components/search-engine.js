6f7a633c77d19bc941f17e9003ef8afa448ded42
Audrey Diez <audrey.diez@gmail.com>
2021-04-20 16:52:10 +0200

    Feature: Search contains all filters at same time

--- assets/js/components/search-engine.js
+++ assets/js/components/search-engine.js
@@ -25,51 +25,100 @@ export function searching(){
     }
 
     recipesFiltered = [];
-    console.log(filtered);
+    //console.log(filtered);
     //console.log(recipes);
     //console.log(recipesFiltered);
 
-    data.recipes.forEach( recipe => {
-        filtered.forEach(filter => {
-            //console.log(filter.value)
+   /* data.recipes.forEach( recipe => {
 
-            if (filter.type === "searchbar" || filter.type === "ingredients-filters"){
-                if (normalizeString(recipe.name).includes(filter.value)) {
-                    recipesFiltered.push(recipe);
+        nbOfFilters = filtered.length;
+    });*/
 
-                }
-                if (normalizeString(recipe.description).includes(filter.value)) {
-                    recipesFiltered.push(recipe);
 
-                }
-                recipe.ingredients.forEach(ingredient => {
-                    if (normalizeString(ingredient.ingredient).includes(filter.value)) {
-                        recipesFiltered.push(recipe);
+    // TODO TEST
+    let newFilters = [];
+    filtered.forEach(filter => {
+        if(filter.value != null){
+            newFilters.push(filter.value);
+        }
+    })
+    //console.log(newFilters);
 
-                    }
-                })
-            }
+    function searchAllAvailable (myRecipe, filtersArray){
 
-            else if (filter.type === "appliances-filters"){
-                if (normalizeString(recipe.appliance).includes(filter.value)) {
-                    recipesFiltered.push(recipe);
-                }
-
-            }
-            else if (filter.type === "ustensils-filters"){
-                recipe.ustensils.forEach(ustensil => {
-                    if (normalizeString(ustensil).includes(filter.value)) {
-                        recipesFiltered.push(recipe);
-
-                    }
-                })
-            }
-
+        return filtersArray.every(function(filter){
+            console.log(filter)
+            return (
+                normalizeString(myRecipe.name).indexOf(filter)>-1
+                || normalizeString(myRecipe.description).indexOf(filter)>-1
+                || normalizeString(myRecipe.appliance).indexOf(filter)>-1
+                || myRecipe.ingredients.filter(ingredient => normalizeString(ingredient.ingredient).indexOf(filter) > -1).length > 0
+                || myRecipe.ustensils.filter(ustensil => normalizeString(ustensil).indexOf(filter) > -1).length > 0
+            )
         })
+    }
+    // TODO TEST
+    //|| normalizeString(myRecipe.description).indexOf(filter)>-1
+    //|| myRecipe.ingredients.filter(ingredient => normalizeString(ingredient.ingredient).indexOf(filter) > -1).length > -1
+
+    data.recipes.forEach( recipe => {
+
+        //let string = normalizeString(recipe.name).toString();
+
+        //console.log(multiSearch(string, newFilters))
+
+        if (searchAllAvailable(recipe, newFilters)){
+            recipesFiltered.push(recipe);
+        }
+
+
+
+
+
+
+
+
+        // filtered.forEach(filter => {
+        //     //console.log(filter.value)
+        //
+        //     if (filter.type === "searchbar" || filter.type === "ingredients-filters"){
+        //         if (normalizeString(recipe.name).includes(filter.value)) {
+        //             recipesFiltered.push(recipe);
+        //
+        //         }
+        //         if (normalizeString(recipe.description).includes(filter.value)) {
+        //             recipesFiltered.push(recipe);
+        //
+        //         }
+        //         recipe.ingredients.forEach(ingredient => {
+        //             if (normalizeString(ingredient.ingredient).includes(filter.value)) {
+        //                 recipesFiltered.push(recipe);
+        //
+        //             }
+        //         })
+        //     }
+        //
+        //     else if (filter.type === "appliances-filters"){
+        //         if (normalizeString(recipe.appliance).includes(filter.value)) {
+        //             recipesFiltered.push(recipe);
+        //         }
+        //
+        //     }
+        //     else if (filter.type === "ustensils-filters"){
+        //         recipe.ustensils.forEach(ustensil => {
+        //             if (normalizeString(ustensil).includes(filter.value)) {
+        //                 recipesFiltered.push(recipe);
+        //
+        //             }
+        //         })
+        //     }
+        //
+        // })
     });
+
     removeNodesRecipes();
     recipesFiltered = recipesFiltered.filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i);
-    console.log(recipesFiltered);
+    //console.log(recipesFiltered);
     //console.log(filtered);
     let filters = displayRecipes(recipesFiltered);
     //console.log(filters)
@@ -88,4 +137,5 @@ export function normalizeString(string) {
     string = string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     return string.toLowerCase();
 }
+
 
